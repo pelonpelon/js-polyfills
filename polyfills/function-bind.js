c4f98973e270acb3ae703cfc931db3adc2a622c9
pelonpelon <github@xlfag.com>
2015-07-30 10:08:27 -0500

    function-bind

--- polyfills/function-bind.js
+++ polyfills/function-bind.js
@@ -1,6 +1,17 @@
 // taken from You Don't Know Javascript via MDN
 // https://github.com/getify/You-Dont-Know-JS/blob/master/this%20%26%20object%20prototypes/ch2.md
 
+/***
+
+Note: The bind(..) polyfill shown below differs from the built-in bind(..) in
+ES5 with respect to hard-bound functions that will be used with new (see below
+for why that's useful). Because the polyfill cannot create a function without
+a .prototype as the built-in utility does, there's some nuanced indirection to
+approximate the same behavior. Tread carefully if you plan to use new with
+a hard-bound function and you rely on this polyfill.
+
+***/
+
 if (!Function.prototype.bind) {
     Function.prototype.bind = function(oThis) {
         if (typeof this !== 'function') {
